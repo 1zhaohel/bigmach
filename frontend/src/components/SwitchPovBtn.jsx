d4fe5dc297ae78cfe7c62e523ed0d7137920bc52
Michelle <michh2005@gmail.com>
2025-01-18 03:04:48 -0500

    Added more tailwind configs

---
+++ frontend/src/components/SwitchPovBtn.jsx
@@ -0,0 +1,12 @@
+// function SwitchPovBtn( {btnText, btnCb} ) {
+//     return (
+//         <button 
+//             onClick={btnCb}
+//             className="px-6 py-2 rounded-full bg-pink-100 text-gray-600 text-lg font-light italic shadow-lg transition hover:shadow-xl"
+//         >
+//             {btnText}
+//         </button>
+//     )
+// }
+
+// export default SwitchPovBtn
