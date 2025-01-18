775d48e4c790e17d7487c13be715a22de6ba969f
AngelChen09 <110699188+AngelChen09@users.noreply.github.com>
2025-01-17 22:57:57 -0500

    feat: added basic dao for gemini

---
+++ backend/GeminiAPI_DAO.py
@@ -0,0 +1,20 @@
+from dotenv import load_dotenv
+import google.generativeai as genai
+import os
+
+class GeminiAPI_DAO:
+    def __init__(self):
+
+        # Load API key from .env.local file
+        load_dotenv('.env.local')
+        self.gemini_api_key = os.getenv('GEMINI_API_KEY')
+
+        # Initialize GeminiAI API
+        genai.configure(api_key=self.gemini_api_key)
+        self.model = genai.GenerativeModel("gemini-1.5-flash")
+        
+
+    def prompt_AI(self, prompt: str):
+        response = self.model.generate_content(prompt)
+        print(response)
+
