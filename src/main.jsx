f682a937a5e8559e845c7a13e7267512ed03122e
Michelle <michh2005@gmail.com>
2025-01-17 22:27:19 -0500

    added auth0 to package.json and created auth0 jsx files

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById('root'));
+
+root.render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Auth0Provider
+        domain="dev-3s1oqzoonl3toxbn.us.auth0.com"
+        clientId="htIXnAJe6NGaeyFoWcP31iIMDBTAtBWW"
+        authorizationParams={{
+          redirect_uri: window.location.origin
+        }}
+      >
+        <App />
+      </Auth0Provider>
+  </StrictMode>
+);
