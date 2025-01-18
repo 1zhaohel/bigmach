92898ef9028c3f2d88f724160c142db24b9be9a9
Michelle <michh2005@gmail.com>
2025-01-18 15:16:04 -0500

    merge changes

--- frontend/src/components/TypingBox.jsx
+++ frontend/src/components/TypingBox.jsx
@@ -4,9 +4,10 @@ function TypingBox({receiver = "xxxx"}) {
     return(
         <div>
             <textarea id="message" rows="1" placeholder={placeholder} className="block p-2.5 w-full text-sm text-gray1 bg-pink4 rounded-lg border border-pink5 resize-none shadow-md " ></textarea>
-            
+
         </div>
 
+        // check org changes
     )
 }
 
