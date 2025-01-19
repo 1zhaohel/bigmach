644fe08ffb75c9b3ac972a99e37d347222f8d48b
Michelle <michh2005@gmail.com>
2025-01-19 02:14:28 -0500

    added options check for cors

--- backend/server.py
+++ backend/server.py
@@ -3,20 +3,24 @@ from flask_cors import CORS
 from dotenv import load_dotenv
 import google.generativeai as genai
 import os
-from backend.GeminiAPI_DAO import GeminiAPI_DAO
+from backend.GeminiAPI_DAO import GeminiAPI_DAO # use this when pushing to heroku
 import json
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": ["http://localhost:5173"]}})
 
-@app.route("/chat", methods=["POST"])
+@app.route("/chat", methods=["POST", "OPTIONS"])
 def chat():
+    if request.method == "OPTIONS":
+        # Handle preflight request (respond to OPTIONS request)
+        return '', 200
+    
     raw_data = request.data
     decoded_data = raw_data.decode('utf-8')
 
     json_data = json.loads(decoded_data)
     
-    test = GeminiAPI_DAO("therapist") # TODO: make this dynamic
+    test = GeminiAPI_DAO("therapist")
     response = test.prompt_AI(json_data["prompt"])
     response = jsonify(response)
 
