89d5caa598f1c52314128b98c2412acf47ed8a95
Michelle <michh2005@gmail.com>
2025-01-19 05:42:30 -0500

    added corecct import

--- backend/server.py
+++ backend/server.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 import google.generativeai as genai
 import os
 # from GeminiAPI_DAO import GeminiAPI_DAO # use backend.GeminiAPI_DAP  when pushing to heroku
-from GeminiAPI_DAO import GeminiAPI_DAO
+from backend.GeminiAPI_DAO import GeminiAPI_DAO
 import json
 
 app = Flask(__name__)
